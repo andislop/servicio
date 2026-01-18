14914a982946f434869080720aa19c4d9b8a12d1
andislop <141179041+andislop@users.noreply.github.com>
2026-01-17 23:10:26 -0400

    config

--- api/src/config/firebase.js
+++ api/src/config/firebase.js
@@ -3,13 +3,23 @@ import { getFirestore } from "firebase-admin/firestore";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-const serviceAccount = require("./firebase-key.json");
+const serviceAccount = {
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  // Reemplazamos los saltos de línea que a veces se rompen en Vercel
+  private_key: process.env.private_key.replace(/\\n/g, '\n'),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.client_x509_cert_url
+};
 
 if (!getApps().length) {
   initializeApp({
     credential: cert(serviceAccount)
   });
 }
-
 const db = getFirestore();
 export default db;
